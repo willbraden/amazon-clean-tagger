081d235b6e172dde451b1eec1ac1ee7101fa95a8
Will Braden <willbraden@gmail.com>
2018-02-26 14:45:54 -0600

    ready function again

--- jquery.amazon-clean-tagger.js
+++ jquery.amazon-clean-tagger.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	var tag = 'wjbt-20';
-$('a[href *= "amazon.com"]').each( function() {
-	var url = $(this).attr('href');
+jQuery('a[href *= "amazon.com"]').each( function() {
+	var url = jQuery(this).attr('href');
 	console.log();
 	var regex = RegExp("https://www.amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})");
 	m = url.match(regex);
 	if (m) { 
 		var newURL = 'https://www.amazon.com/dp/' + m[4] + '/&tag=' + tag;
-	    $(this).attr('href',newURL);
+	    jQuery(this).attr('href',newURL);
 	}
 });
 
